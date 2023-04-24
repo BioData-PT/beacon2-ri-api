e02361265441dafd1e3cbc67a842a57d2781eaee
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2023-04-24 16:18:14 +0100

    Added confirmation on indexing script

--- deploy/reindex.py
+++ deploy/reindex.py
@@ -13,7 +13,7 @@ client = MongoClient(
         conf.database_auth_source,
     )
 )
-
+print("*** Starting to reindex... ***")
 client.beacon.analyses.create_index([("$**", "text")])
 client.beacon.biosamples.create_index([("$**", "text")])
 client.beacon.cohorts.create_index([("$**", "text")])
@@ -21,3 +21,4 @@ client.beacon.datasets.create_index([("$**", "text")])
 client.beacon.genomicVariations.create_index([("$**", "text")])
 client.beacon.individuals.create_index([("$**", "text")])
 client.beacon.runs.create_index([("$**", "text")])
+print("*** Reindex completed OK! ***")
