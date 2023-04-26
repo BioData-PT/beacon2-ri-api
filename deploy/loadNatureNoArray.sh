67ce8947612942d371f68214f20fbb3eecb001ee
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2023-04-26 10:54:56 +0100

    Script to load non-array data (e.g. from backup dumps), fixed static fields

---
+++ deploy/loadNatureNoArray.sh
@@ -0,0 +1,19 @@
+natureData=../../natureDataFiles
+metadataDir=$natureData/april2023
+genomicDataDir=$natureData/april2023
+#metadataDir=data/mydata/cineca
+#genomicDataDir=data/mydata/cineca
+
+DB_PASSWD="example"
+source secret.py # import db passwd
+
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/analyses*.json --collection analyses
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/biosamples*.json --collection biosamples
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/cohorts*.json --collection cohorts
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/datasets*.json --collection datasets
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/individuals*.json --collection individuals
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $metadataDir/runs*.json --collection runs
+
+mongoimport --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $genomicDataDir/genomicVariations*.json --collection genomicVariations
+#mongoimport --jsonArray --uri "mongodb://root:$DB_PASSWD@127.0.0.1:27017/beacon?authSource=admin" --file $genomicDataDir/genomicVariations*.json.gz --collection genomicVariations
+
