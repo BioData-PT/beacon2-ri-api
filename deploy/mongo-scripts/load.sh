1bece183d1851584edcc97e18d98be5b8afa2672
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-06-15 11:59:08 +0100

    fix: import data

--- deploy/mongo-scripts/load.sh
+++ deploy/mongo-scripts/load.sh
@@ -2,5 +2,5 @@
   do
     mongoimport --jsonArray \
     --uri "mongodb://root:${DB_PASSWD}@127.0.0.1:27017/beacon?authSource=admin" \
-		--file input_data/cineca_with_datasetid/analyses*.json --collection $${collection} ;
+		--file input_data/cineca_with_datasetid/${collection}.json --collection ${collection} ;
 	done
