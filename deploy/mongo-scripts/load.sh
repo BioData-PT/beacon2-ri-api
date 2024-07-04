6450ef98fc44c1aa2ab856e54fd853a7da4b4c95
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-04 13:29:28 +0100

    allele frequencies start

--- deploy/mongo-scripts/load.sh
+++ deploy/mongo-scripts/load.sh
@@ -4,3 +4,5 @@
     --uri "mongodb://root:${DB_PASSWD}@127.0.0.1:27017/beacon?authSource=admin" \
 		--file input_data/cineca_with_datasetid/${collection}.json --collection ${collection} ;
 	done
+
+python3 update_allele_frequencies.py
---
+++ deploy/mongo-scripts/update_allele_frequencies.py
@@ -0,0 +1,43 @@
+import requests
+from pymongo import MongoClient
+
+# function to format a single variant
+def format_variant_for_search(variant):
+    chromosome = variant["_position"]["refseqId"]
+    start_position = variant["_position"]["startInteger"]
+    reference_base = variant["variation"]["referenceBases"]
+    alternate_base = variant["variation"]["alternateBases"]
+
+    formatted_variant = f"{chromosome}-{start_position}-{reference_base}-{alternate_base}"
+    return formatted_variant
+
+# function to query gnomAD for allele frequency
+def query_gnomad(formatted_variant):
+    url = f"https://gnomad.broadinstitute.org/api/variant/{formatted_variant}?dataset=gnomad_r4"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        allele_frequency = data.get("variant", {}).get("allele_freq", None)
+        return allele_frequency
+    return None
+
+# connect to MongoDB
+client = MongoClient('mongodb://root:your_database_password@127.0.0.1:27017/beacon?authSource=admin')
+db = client['beacon']
+collection = db['genomicVariations']
+
+# iterate over all variants, format them, query gnomAD, and update the database
+for variant in collection.find():
+    formatted_variant = format_variant_for_search(variant)
+    print(formatted_variant)
+    #allele_frequency = query_gnomad(formatted_variant)
+    #if allele_frequency is not None:
+     #   collection.update_one(
+      #      {"variantInternalId": variant["variantInternalId"]},
+       #     {"$set": {"allele_frequency": allele_frequency}}
+        #)
+        #print(f"Updated variant {formatted_variant} with allele frequency {allele_frequency}")
+    #else:
+     #   print(f"Failed to retrieve allele frequency for {formatted_variant}")
+
+print("Finished updating allele frequencies.")
