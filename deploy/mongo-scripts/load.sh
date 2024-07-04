301bed4289a16921381fd574f5ca96d72312764c
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-04 13:55:29 +0100

    editing script 5

--- deploy/mongo-scripts/load.sh
+++ deploy/mongo-scripts/load.sh
@@ -5,5 +5,22 @@
 		--file input_data/cineca_with_datasetid/${collection}.json --collection ${collection} ;
 	done
 
+# Check if python3 is installed
+PYTHON3_PATH=$(which python3)
+if [ -z "$PYTHON3_PATH" ]; then
+    echo "python3 is not installed or not in the PATH."
+    exit 1
+fi
+
+# Activate virtual environment if it exists
+if [ -d "venv" ]; then
+    source venv/bin/activate
+fi
+
 # python script to update allele frequencies in genomicVariantions collection
 python3 update_allele_frequencies.py
+
+# Deactivate virtual environment if it was activated
+if [ -d "venv" ]; then
+    deactivate
+fi
