d97335ea014c5e8a3482b33f11ab6aa8f68b778d
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-04 14:17:11 +0100

    editing script 9

--- deploy/mongo-scripts/load.sh
+++ deploy/mongo-scripts/load.sh
@@ -4,15 +4,3 @@
     --uri "mongodb://root:${DB_PASSWD}@127.0.0.1:27017/beacon?authSource=admin" \
 		--file input_data/cineca_with_datasetid/${collection}.json --collection ${collection} ;
 	done
-
-# Check if python3 is installed and in the PATH
-PYTHON3_PATH=$(which python3)
-if [ -z "$PYTHON3_PATH" ]; then
-    echo "python3 is not installed or not in the PATH."
-    exit 1
-else
-    echo "Found python3 at $PYTHON3_PATH"
-fi
-
-# python script to update allele frequencies in genomicVariantions collection
-python3 update_allele_frequencies.py
