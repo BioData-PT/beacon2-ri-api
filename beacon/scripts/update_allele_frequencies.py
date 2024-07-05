91d936c72e4c6362b0b419ee2547ae59a133e2cd
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-05 16:35:28 +0100

    trying to get allele frequencies 37-38 mapping 24

--- beacon/scripts/update_allele_frequencies.py
+++ beacon/scripts/update_allele_frequencies.py
@@ -5,8 +5,8 @@ import time
 
 # Function to query 1000 Genomes for allele frequency
 def query_1000_genomes(chrom, start, end, ref, alt):
-    print("START " + start)
-    print("END" + end)
+    print("START " + f"{start}")
+    print("END " + f"{end}")
     server = "https://rest.ensembl.org"
     ext = f"/map/human/GRCh37/{chrom}:{start}..{end}:1/GRCh38?"
  
