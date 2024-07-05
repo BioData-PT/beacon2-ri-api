790c037130bea3c2fafdef2281dda38febd66f2a
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-05 12:09:41 +0100

    trying to get allele frequencies gnomad again 5

--- beacon/scripts/update_allele_frequencies.py
+++ beacon/scripts/update_allele_frequencies.py
@@ -57,7 +57,7 @@ for variant in collection.find():
     print("-----------")
     print(f"{formatted_variant}")
     chrom, pos, ref, alt = formatted_variant.split('-')
-    allele_frequency = query_gnomad(chrom, pos, ref, alt)
+    allele_frequency = query_gnomad(int(chrom), pos, ref, alt)
     if allele_frequency is not None:
         collection.update_one(
             {"variantInternalId": variant["variantInternalId"]},
