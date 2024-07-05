9f4ecdb51485fe2a48570d45d57b48512b83c585
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-05 12:14:23 +0100

    trying to get allele frequencies gnomad again 7

--- beacon/scripts/update_allele_frequencies.py
+++ beacon/scripts/update_allele_frequencies.py
@@ -10,13 +10,16 @@ def format_variant_for_search(variant):
     formatted_variant = f"{chromosome}-{start_position}-{reference_base}-{alternate_base}"
     return formatted_variant
 
-# function to query gnomAD for allele frequency
+# function to query for allele frequency
 def query_1000_genomes(chrom, pos, ref, alt):
     query_url = f"http://grch37.rest.ensembl.org/variation/human/{chrom}_{pos}_{ref}_{alt}?"
     headers = {"Content-Type": "application/json"}
     response = requests.get(query_url, headers=headers)
     if response.status_code == 200:
         return response.json()
+    elif response.status_code == 400:
+        print(f"Bad request for variant {chrom}-{pos}-{ref}-{alt}: {response.text}")
+        return None
     else:
         raise Exception(f"Query failed to run with a {response.status_code}.")
 
@@ -33,14 +36,16 @@ client = MongoClient(
         "admin"
     )
 )
+
 collection = client.beacon.get_collection('genomicVariations')
+
 # iterate over all variants, format them, query gnomAD, and update the database
 for variant in collection.find():
     formatted_variant = format_variant_for_search(variant)
     print("-----------")
     print(f"{formatted_variant}")
     chrom, pos, ref, alt = formatted_variant.split('-')
-    allele_frequency = query_1000_genomes(chrom, pos, ref, alt)
+    allele_frequency = query_1000_genomes(chrom, int(pos), ref, alt)
     print(allele_frequency)
     if allele_frequency is not None:
         collection.update_one(
