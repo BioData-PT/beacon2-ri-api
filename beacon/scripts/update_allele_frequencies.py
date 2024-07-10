2eeb64f96de07902f0459eb44518c67e0f40e651
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-10 20:05:35 +0100

    allele frequencies done

--- beacon/scripts/update_allele_frequencies.py
+++ beacon/scripts/update_allele_frequencies.py
@@ -1,28 +1,23 @@
-import requests
-import sys
-from pymongo import MongoClient, UpdateOne
+import requests, sys
+from pymongo import MongoClient
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
-import threading
-import hashlib
 
-# Lock for threading
-lock = threading.Lock()
 
-# Function to compute the complement of a sequence
 def complement(sequence):
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join(complement[base] for base in sequence)
 
-# Function to query 1000 Genomes for allele frequency
+
+# function to query 1000 Genomes for allele frequency
 def query_1000_genomes(chrom, start, end, ref, alt, type):
     server = "https://rest.ensembl.org"
     ext = f"/map/human/GRCh37/{chrom}:{start}..{end}:1/GRCh38?"
-    
+ 
     r = requests.get(server + ext, headers={"Content-Type": "application/json"})
-    
+ 
     if not r.ok:
-        return None
+        r.raise_for_status()
+        sys.exit()
 
     decoded = r.json()
     mappings = decoded['mappings']
@@ -35,25 +30,35 @@ def query_1000_genomes(chrom, start, end, ref, alt, type):
     j = re.json()
     check = j['seq']
     
+    
+    # construct the HGVS notation
     if type == 'INDEL':
         hgvs_notation = f"{chrom}:g.{mapped_start}_{mapped_end}del{complement(ref)}ins{complement(alt)}"
     elif check == complement(alt):
-        hgvs_notation = f"{chrom}:g.{mapped_start}{complement(ref)}>{complement(alt)}"
-    elif 'most_severe_consequence' in mapped_data and mapped_data['most_severe_consequence'] == 'downstream_gene_variant':
         hgvs_notation = f"{chrom}:g.{mapped_start}{complement(alt)}>{complement(ref)}"
+    elif 'most_severe_consequence' in  mapped_data:
+        if mapped_data['most_severe_consequence'] == 'downstream_gene_variant':
+            hgvs_notation = f"{chrom}:g.{mapped_start}{complement(alt)}>{complement(ref)}"
     else:
         hgvs_notation = f"{chrom}:g.{mapped_start}{complement(ref)}>{complement(alt)}"
     
+    # construct the URL for Ensembl VEP
     url = f"https://rest.ensembl.org/vep/human/hgvs/{hgvs_notation}?"
-    
+ 
+    # make GET request to the API
     response = requests.get(url, headers={"Content-Type": "application/json"})
-    
+ 
+    # see if request was successful
     if response.status_code == 200:
-        return response.json()
+        json_response = response.json()
+        print(f"GRCh38 + {hgvs_notation}")
+        return json_response
     else:
+        print(f"Bad request for variant {chrom}-{start}-{ref}-{alt}: {response.text}")
         return None
-
-# Connect to MongoDB
+    
+    
+# connect to MongoDB
 database_password = os.getenv('DB_PASSWD')
 client = MongoClient(
     "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
@@ -65,10 +70,13 @@ client = MongoClient(
         "admin"
     )
 )
+
+
 collection = client.beacon.get_collection('genomicVariations')
 
-# Function to process a single variant
-def process_variant(variant):
+
+# iterate over all variants, format them, query 1000 Genomes, and update the database
+for variant in collection.find():
     chromosome = variant["_position"]["refseqId"]
     start_position = variant["_position"]["startInteger"]
     end_position = variant["_position"]["endInteger"]
@@ -76,12 +84,11 @@ def process_variant(variant):
     alternate_base = variant["variation"]["alternateBases"]
     variant_type = variant["variation"]['variantType']
     
-    with lock:
-    
-        formatted_variant = f"{chromosome}-{start_position}-{end_position}-{reference_base}-{alternate_base}"
-        print("------------------------------------")
-        print(f"Variant + {formatted_variant}")
-        
+    formatted_variant = f"{chromosome}-{start_position}-{end_position}-{reference_base}-{alternate_base}"
+    print("------------------------------------")
+    print(f"Variant + {formatted_variant}")
+    try:
+        # query 1000 Genomes for allele frequency
         allele_frequency = query_1000_genomes(chromosome, start_position, end_position, reference_base, alternate_base, variant_type)
         
         if allele_frequency is not None:
@@ -97,37 +104,29 @@ def process_variant(variant):
                         elif "gnomadg" not in data[key] and "af" in data[key]:
                             total_frequency = data[key]["af"]
                         else:
-                            total_frequency = 1 / collection.count_documents({})
+                            total_frequency = 1/collection.count_documents({}) # allele frequency in the beacon database
                         if total_frequency == 0:
-                            total_frequency = 1 / collection.count_documents({})
-                    
-                        print(f"Updated variant {formatted_variant} with allele frequency {total_frequency}")
-                    
-                        return UpdateOne(
-                            {"variantInternalId": variant["variantInternalId"]},
-                            {"$set": {"alleleFrequency": total_frequency}}
-                        )
+                            total_frequency = 1/collection.count_documents({}) # allele frequency in the beacon database
+                    collection.update_one(
+                        {"variantInternalId": variant["variantInternalId"]},
+                        {"$set": {"alleleFrequency": total_frequency}}
+                    )
+                else:
+                    total_frequency = 1/collection.count_documents({}) # allele frequency in the beacon database
+            else:
+                total_frequency = 1/collection.count_documents({}) # allele frequency in the beacon database
+            print(f"Updated variant {formatted_variant} with allele frequency {total_frequency}")
+            
+        else:
+           print(f"Failed to retrieve allele frequency for {formatted_variant}")
+           
+    except ValueError as e:
+        print(str(e))
+        continue
     
-    return None
-
-# Main script to process all variants
-def main():
-    update_requests = []
-    futures = []
-
-    with ThreadPoolExecutor(max_workers=10) as executor:
-        for variant in collection.find():
-            future = executor.submit(process_variant, variant)
-            futures.append(future)
+    except Exception as e:
         
-        for future in as_completed(futures):
-            update_request = future.result()
-            if update_request:
-                update_requests.append(update_request)
+        print(f"Failed to process variant {formatted_variant}: {e}")
+        continue
     
-    if update_requests:
-        collection.bulk_write(update_requests)
-    print("Finished updating allele frequencies.")
-
-if __name__ == "__main__":
-    main()
+print("Finished updating allele frequencies.")
