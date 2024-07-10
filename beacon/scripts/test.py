db37317262ef1bf7f0c1782cc94a6f796ec7426e
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-10 16:42:39 +0100

    complement

--- beacon/scripts/test.py
+++ beacon/scripts/test.py
@@ -3,4 +3,4 @@ def reverse_complement(sequence):
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     return ''.join(complement[base] for base in sequence[::-1])
 
-reverse_complement("CT")
+print(reverse_complement("CT"))
