6e2c8c02d74addb4a0bdaa0bdf39da7c0ace4aa5
FracassandoCasualmente <miguel.conrado.santos@tecnico.ulisboa.pt>
2024-07-10 19:46:35 +0100

    complement

--- beacon/scripts/update_allele_frequencies.py
+++ beacon/scripts/update_allele_frequencies.py
@@ -36,7 +36,7 @@ def query_1000_genomes(chrom, start, end, ref, alt, type):
         hgvs_notation = f"{chrom}:g.{mapped_start}_{mapped_end}del{complement(ref)}ins{complement(alt)}"
     elif check == complement(alt):
         print(f"CHECKKKKKKK + {check}")
-        hgvs_notation = f"{chrom}:g.{mapped_start}{complement(ref)}>{complement(alt)}"
+        hgvs_notation = f"{chrom}:g.{mapped_start}{complement(alt)}>{complement(ref)}"
     elif 'most_severe_consequence' in  mapped_data:
         if mapped_data['most_severe_consequence'] == 'downstream_gene_variant':
             hgvs_notation = f"{chrom}:g.{mapped_start}{complement(alt)}>{complement(ref)}"
