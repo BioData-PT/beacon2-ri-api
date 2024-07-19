1a5b7d5b33cb30307a12554e6b60d722d120729f
sara-santana <saraamaralsantana@gmail.com>
2024-07-19 15:39:27 +0100

    fixing errors

--- beacon/response/build_response.py
+++ beacon/response/build_response.py
@@ -69,7 +69,6 @@ def build_generic_response(
         }
         
         # if dataset is not authorized, erase the records part
-        LOG.debug(f"DATASETS = {accessible_datasets}")
         if dataset_id not in accessible_datasets:
             dataset_response["results"] = []
             
