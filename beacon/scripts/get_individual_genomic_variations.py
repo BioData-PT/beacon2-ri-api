b52dfd06e79a53e648ef9c9f6cd99399b9bf2fe4
sara-santana <saraamaralsantana@gmail.com>
2024-07-20 12:16:52 +0100

    trying the script of attacker

--- beacon/scripts/get_individual_genomic_variations.py
+++ beacon/scripts/get_individual_genomic_variations.py
@@ -1,21 +1,23 @@
 import os
+import subprocess
 from pymongo import MongoClient
 
-def get_genomic_variants_for_individual(biosample_id):
-    # connect to MongoDB
-    database_password = os.getenv('DB_PASSWD')
-    client = MongoClient(
-        "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
-            "root",
-            database_password,
-            "db",
-            27017,
-            "beacon",
-            "admin"
-        )
-    )
+# connect to MongoDB
+database_password = os.getenv('DB_PASSWD')
 
-    collection = client.beacon.get_collection('genomicVariations')
+client = MongoClient(
+    "mongodb://{}:{}@{}:{}/{}?authSource={}".format(
+        "root",
+        database_password,
+        "db",
+        27017,
+        "beacon",
+        "admin"
+    )
+)
+collection = client.beacon.get_collection('genomicVariations')
+
+def get_genomic_variants_for_individual(biosample_id):
 
     # query to find all genomic variants for the given biosampleId = individualId
     query = {'caseLevelData.biosampleId': biosample_id}
@@ -28,16 +30,61 @@ def get_genomic_variants_for_individual(biosample_id):
 
     return variant_ids
 
+def query_variant_with_curl(access_token, alt, ref, start, end, vType):
+    # Construct the curl command
+    curl_command = [
+        'curl',
+        '-H', 'Content-Type: application/json',
+        '-X', 'POST',
+        '-H', f"Authorization: Bearer {access_token}",
+        '-d', f'''{{
+            "meta": {{
+                "apiVersion": "2.0"
+            }},
+            "query": {{
+                "requestParameters": {
+                f"alternateBases: {alt}" ,
+                f"referenceBases: {ref}" ,
+                f"start: [ {start} ]",
+                f"end: [ {end} ]",
+                f"variantType: {vType}"
+        },,
+                "filters": [],
+                "includeResultsetResponses": "HIT",
+                "pagination": {{
+                    "skip": 0,
+                    "limit": 10
+                }},
+                "testMode": false,
+                "requestedGranularity": "record"
+            }}
+        }}''',
+        'http://localhost:5050/api/g_variants/'
+    ]
+
+    # Execute the curl command and capture the output
+    result = subprocess.run(curl_command, capture_output=True, text=True)
+    return result.stdout, result.stderr
+
 def main():
-    # ask which individual we will try to attack
+    access_token = input("Enter the access token: ")
     biosample_id = input("Enter the individualId to search: ")
     variant_ids = get_genomic_variants_for_individual(biosample_id)
     
-    # try and query the all the variants starting with the lower frequency ones and see when the individual is removed from the output
+    # Try and query all the variants starting with the lower frequency ones and see when the individual is removed from the output
     if variant_ids:
         print(f"The genomic variants for biosampleId {biosample_id} are (sorted by alleleFrequency):")
         for vid in variant_ids:
-            print(vid)
+            print(f"Querying variant id: {vid}")
+            alt = collection.find({'variantInternalId': vid})["variation"]["alternateBases"]
+            ref = collection.find({'variantInternalId': vid})["variation"]["referenceBases"]
+            start = collection.find({'variantInternalId': vid})["_position"]["startInteger"]
+            end = collection.find({'variantInternalId': vid})["_position"]["endInteger"]
+            vType = collection.find({'variantInternalId': vid})["variation"]['variantType']
+            stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
+            print("Response:", stdout)
+            if stderr:
+                print("Error:", stderr)
     else:
         print(f"No genomic variants found for individualId {biosample_id}")
 
