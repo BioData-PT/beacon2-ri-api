fa09dd7e29fa936b48c1ec11e22406d06260140e
sara-santana <saraamaralsantana@gmail.com>
2024-08-01 18:26:11 +0100

    handling removed individuals

--- beacon/request/handlers.py
+++ beacon/request/handlers.py
@@ -157,6 +157,8 @@ def pvalue_strategy(access_token, records, qparams):
             for individual in individuals_to_remove:
                 LOG.debug(f"The individual with id {individual} was removed from the output") # signal to know when an individual has no more budget left
             record['caseLevelData'] = [case for case in record['caseLevelData'] if case.get('biosampleId') not in individuals_to_remove]
+            if not case_level_data:
+                records.remove(record)
 
     return None, records
 
