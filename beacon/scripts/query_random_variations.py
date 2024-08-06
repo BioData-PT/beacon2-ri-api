d19165a874665fd3621bd30d742deaa5d22cca9d
sara-santana <saraamaralsantana@gmail.com>
2024-08-06 11:07:20 +0100

    preparing for scripts

--- beacon/scripts/query_random_variations.py
+++ beacon/scripts/query_random_variations.py
@@ -100,7 +100,7 @@ def main():
             vType = variant_full_doc["variation"]['variantType']
             stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
             
-            if stdout == True:
+            if stdout:
                 print(f"Individuals were removed")
                 break
             
