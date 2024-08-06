aa8a1fa0be45abe0fbce8406a4d79e3fd9820aa0
sara-santana <saraamaralsantana@gmail.com>
2024-08-06 18:04:23 +0100

    running scripts

--- beacon/scripts/query_random_variations.py
+++ beacon/scripts/query_random_variations.py
@@ -101,6 +101,7 @@ def main():
                 end = variant_full_doc["_position"]["endInteger"]
                 vType = variant_full_doc["variation"]['variantType']
                 stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
+                print(f"Walk number: {i}")
                 
                 print(stdout)
                 if stdout == "true":
