4ee1bb5d89a309a85f2cb98b7d755e4c62c76cc4
sara-santana <saraamaralsantana@gmail.com>
2024-08-07 14:11:21 +0100

    running to see time

--- beacon/scripts/query_100_different_variations.py
+++ beacon/scripts/query_100_different_variations.py
@@ -23,15 +23,7 @@ def clear_budget_and_history_collections():
     client.beacon.get_collection('budget').delete_many({})
     client.beacon.get_collection('history').delete_many({})
     print("Cleared budget and history collections.")
-    
-def get_random_genomic_variants(exclude_ids, sample_size=1):
-    # Get a random sample of genomic variants excluding already queried ones
-    pipeline = [
-        {"$match": {"variantInternalId": {"$nin": exclude_ids}}},
-        {"$sample": {"size": sample_size}},
-        {"$project": {"variantInternalId": 1, "alleleFrequency": 1}}
-    ]
-    return list(collection.aggregate(pipeline))
+
 
 def query_variant_with_curl(access_token, alt, ref, start, end, vType):
     # Construct the curl command
@@ -71,40 +63,43 @@ def query_variant_with_curl(access_token, alt, ref, start, end, vType):
 
 def main():
     access_token = input("Enter the access token: ")
-    queried_variant_ids = set()  # To track queried variants
+    queried_variant_ids = {'chr22_16139740_A_G', 'chr22_16136506_G_T', 'chr22_16064261_C_T', 'chr22_16063481_TAA_T', 'chr22_16133452_G_C', 'chr22_16058056_C_T', 
+                           'chr22_16123353_G_A', 'chr22_16059343_G_A', 'chr22_16062988_C_T', 'chr22_16065307_C_T', 'chr22_16054424_C_T', 'chr22_16055127_G_A', 
+                           'chr22_16086492_T_G', 'chr22_16050954_G_A', 'chr22_16119409_G_A', 'chr22_16057172_G_A', 'chr22_16135992_G_C', 'chr22_16063219_C_G', 
+                           'chr22_16053127_C_T', 'chr22_16057850_G_A', 'chr22_16096647_C_T', 'chr22_16085285_G_A', 'chr22_16057320_G_A', 'chr22_16064133_T_G', 
+                           'chr22_16070681_T_C', 'chr22_16123467_C_T', 'chr22_16138596_A_G', 'chr22_16054248_C_A', 'chr22_16135601_T_A', 'chr22_16053458_G_C', 
+                           'chr22_16050115_G_A', 'chr22_16071624_A_G', 'chr22_16126752_C_T', 'chr22_16123403_C_T', 'chr22_16080425_T_C', 'chr22_16061902_C_A', 
+                           'chr22_16057417_C_T', 'chr22_16067722_C_T', 'chr22_16081610_A_G', 'chr22_16081589_C_T', 'chr22_16058809_C_T', 'chr22_16054646_T_C', 
+                           'chr22_16056801_G_T', 'chr22_16059009_G_C', 'chr22_16066781_T_C', 'chr22_16115378_T_A', 'chr22_16110847_C_A', 'chr22_16069481_G_A', 
+                           'chr22_16069783_T_A', 'chr22_16071205_A_G', 'chr22_16115374_C_G', 'chr22_16052872_G_C', 'chr22_16055268_G_A', 'chr22_16123182_C_T', 
+                           'chr22_16061872_C_T', 'chr22_16096583_G_C', 'chr22_16063314_C_T', 'chr22_16063513_T_C', 'chr22_16115047_A_G', 'chr22_16122898_G_C', 
+                           'chr22_16065812_A_G', 'chr22_16081593_G_C', 'chr22_16098399_G_C', 'chr22_16050678_C_T', 'chr22_16114253_C_T', 'chr22_16056725_C_A', 
+                           'chr22_16055294_G_A', 'chr22_16130498_A_T', 'chr22_16120001_G_A', 'chr22_16056518_G_A', 'chr22_16061949_G_C', 'chr22_16073412_G_A', 
+                           'chr22_16069307_G_A', 'chr22_16060627_C_T', 'chr22_16062837_C_T', 'chr22_16054839_G_A', 'chr22_16115182_T_A', 'chr22_16072330_G_A', 
+                           'chr22_16053812_T_C', 'chr22_16065004_G_A', 'chr22_16062297_C_T', 'chr22_16052097_G_A', 'chr22_16064462_C_A', 'chr22_16059063_A_C', 
+                           'chr22_16053260_T_C', 'chr22_16053659_A_C', 'chr22_16059952_G_A', 'chr22_16056936_C_T', 'chr22_16078632_G_A', 'chr22_16136556_G_A', 
+                           'chr22_16059796_A_C', 'chr22_16050972_G_A', 'chr22_16066033_G_T', 'chr22_16129025_C_T', 'chr22_16073296_G_C', 'chr22_16127413_G_A', 
+                           'chr22_16051477_C_A', 'chr22_16067164_G_A', 'chr22_16052384_G_C', 'chr22_16055725_G_A'}
     
     start_time = time.time()
-    count = 0
-    
-    while count < 100:
         
-        # Clear budget and history collections before starting queries
-        # clear_budget_and_history_collections()
+    # Clear budget and history collections before starting queries
+    clear_budget_and_history_collections()
             
-        variant_docs = get_random_genomic_variants(list(queried_variant_ids))
-            
-        if not variant_docs:
-            print("No genomic variants found in the database.")
-            break
         
-        for variant_doc in variant_docs:
-            queried_variant_ids.add(variant_doc['variantInternalId'])  # Add to queried set
-            
-            print(f"Variant number: {count}")
-            print(f"Querying variant id: {variant_doc['variantInternalId']}")
-            variant_full_doc = collection.find_one({'variantInternalId': variant_doc['variantInternalId']})
-            alt = variant_full_doc["variation"]["alternateBases"]
-            ref = variant_full_doc["variation"]["referenceBases"]
-            start = variant_full_doc["_position"]["startInteger"]
-            end = variant_full_doc["_position"]["endInteger"]
-            vType = variant_full_doc["variation"]['variantType']
-            stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
-            
-            print(stdout)
-            if stdout == "true":
-                print(f"Individuals were removed")
-            
-        count += 1
+    for variant_id in queried_variant_ids:
+        
+        print(f"Querying variant id: {variant_id}")
+        variant_full_doc = collection.find_one({'variantInternalId': variant_id})
+        alt = variant_full_doc["variation"]["alternateBases"]
+        ref = variant_full_doc["variation"]["referenceBases"]
+        start = variant_full_doc["_position"]["startInteger"]
+        end = variant_full_doc["_position"]["endInteger"]
+        vType = variant_full_doc["variation"]['variantType']
+        stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
+        
+        print(stdout)
+        
         
     end_time = time.time()  # End the timer
     
