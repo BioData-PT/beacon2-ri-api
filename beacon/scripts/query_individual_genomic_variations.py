787c3dd389cb7dc2110570c1c9df3c61f4d8fe8b
sara-santana <saraamaralsantana@gmail.com>
2024-08-13 14:04:16 +0100

    trying again the attacker

--- beacon/scripts/query_individual_genomic_variations.py
+++ beacon/scripts/query_individual_genomic_variations.py
@@ -24,6 +24,16 @@ def clear_budget_and_history_collections():
     client.beacon.get_collection('history').delete_many({})
     print("Cleared budget and history collections.")
     
+individuals_collection = client.beacon.get_collection('genomicVariations')
+    
+def get_random_individual(exclude_ids=set()):
+    # Get a random individual document excluding already selected ones
+    pipeline = [
+        {"$match": {"_id": {"$nin": list(exclude_ids)}}},
+        {"$sample": {"size": 1}}
+    ]
+    return list(individuals_collection.aggregate(pipeline))
+    
 
 def get_genomic_variants_for_individual(biosample_id):
 
@@ -77,32 +87,48 @@ def query_variant_with_curl(access_token, alt, ref, start, end, vType):
 
 def main():
     access_token = input("Enter the access token: ")
-    biosample_id = input("Enter the individualId to search: ")
     
-    # Clear budget and history collections before starting queries
-    clear_budget_and_history_collections()
+    queried_individual_ids = set()
     
-    variant_ids = get_genomic_variants_for_individual(biosample_id)
+    while i < 100:
+        count = 1
+        var = 1
     
-    # Try and query all the variants starting with the lower frequency ones and see when the individual is removed from the output
-    if variant_ids:
-        print(f"The genomic variants for biosampleId {biosample_id} are (sorted by alleleFrequency):")
-        for vid in variant_ids:
-            variant_doc = collection.find_one({'variantInternalId': vid})
-            print(f"Querying variant id: {vid}")
-            alt = variant_doc["variation"]["alternateBases"]
-            ref = variant_doc["variation"]["referenceBases"]
-            start = variant_doc["_position"]["startInteger"]
-            end = variant_doc["_position"]["endInteger"]
-            vType = variant_doc["variation"]['variantType']
-            stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
-            print("Response:", stdout)
-            if biosample_id in stdout:
-                print("The individual was removed")
-            if stderr:
-                print("Error:", stderr)
-    else:
-        print(f"No genomic variants found for individualId {biosample_id}")
+        # Clear budget and history collections before starting queries
+        clear_budget_and_history_collections()
+        
+        individual = get_random_individual(queried_individual_ids)["id"]
+        queried_individual_ids.add(individual['_id'])
+        
+        while var == 1:
+        
+            variant_ids = get_genomic_variants_for_individual(individual)
+            
+            # Try and query all the variants starting with the lower frequency ones and see when the individual is removed from the output
+            if variant_ids:
+                print(f"Variant number: {count}")
+                count += 1
+                print(f"The genomic variants for biosampleId {individual} are (sorted by alleleFrequency):")
+                for vid in variant_ids:
+                    variant_doc = collection.find_one({'variantInternalId': vid})
+                    print(f"Querying variant id: {vid}")
+                    alt = variant_doc["variation"]["alternateBases"]
+                    ref = variant_doc["variation"]["referenceBases"]
+                    start = variant_doc["_position"]["startInteger"]
+                    end = variant_doc["_position"]["endInteger"]
+                    vType = variant_doc["variation"]['variantType']
+                    stdout, stderr = query_variant_with_curl(access_token, alt, ref, start, end, vType)
+                    print("Removed individuals:", stdout)
+                    if individual in stdout:
+                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        print("The individual was removed")
+                        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                        var = 0
+                        break
+                    if stderr:
+                        print("Error:", stderr)
+                        
+        i += 1
 
 if __name__ == "__main__":
     main()
