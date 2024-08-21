a7fb5d9a23de57874a2bd295bcececaee460a2b2
sara-santana <saraamaralsantana@gmail.com>
2024-08-21 15:21:58 +0100

    attacker tester

--- beacon/request/handlers.py
+++ beacon/request/handlers.py
@@ -99,7 +99,7 @@ def pvalue_strategy(access_token, records, qparams):
 
         # step 4: compute the risk for that query: ri = -log(1 - Di)
         allele_frequency = record.get('alleleFrequency')
-        N = client.beacon.get_collection('individuals').count_documents({})  # total number of individuals !! if user requestes dataset, N = individuals in that dataset
+        N = 10000 #client.beacon.get_collection('individuals').count_documents({})  # total number of individuals !! if user requestes dataset, N = individuals in that dataset
         Di = (1 - allele_frequency) ** (2 * N)
         ri = -(math.log10(1 - Di))
         LOG.debug(f"O CUSTO DESTA QUERY É ESTE = {ri}")
@@ -128,7 +128,7 @@ def pvalue_strategy(access_token, records, qparams):
             # Step 3: check if there are records with bj > ri
             budget_info = client.beacon['budget'].find_one(search_criteria)
             if not budget_info:
-                p_value = 0.1 # upper bound on test errors
+                p_value = 0.5 # upper bound on test errors
                 bj = -(math.log10(p_value))  # initial budget
                 budget_info = {
                     "userId": access_token,
